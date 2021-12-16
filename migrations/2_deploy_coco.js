ebdbdbf434152324332013b2c12745bd28aaeafd
10EastSea <xgf6580@gmail.com>
2021-12-16 10:44:52 +0900

    add swap-core

--- migrations/2_deploy_coco.js
+++ migrations/2_deploy_coco.js
@@ -1,5 +1,26 @@
 var COCOToken = artifacts.require("./COCOToken.sol");
+var HEIMToken = artifacts.require("./HEIMToken.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(COCOToken, "COCO Token", "COCO", 18);
+var Factory = artifacts.require("./swap/core/UniswapV2Factory.sol");
+
+module.exports = async function(deployer, network, accounts) {
+  // 토큰 배포
+  await deployer.deploy(COCOToken, "COCO Token", "COCO", 18);
+  await deployer.deploy(HEIMToken, "HEIM Token", "HEIM", 18);
+
+  // 팩토리 배포 (core)
+  await deployer.deploy(Factory, accounts[0]);
+  const factory = await Factory.deployed();
+
+  let cocoAddress, heimAddress;
+  if(network === "mainnet") {
+    cocoAddress = "";
+    heimAddress = "";
+  } else {
+    const cocoToken = await COCOToken.deployed();
+    const heimToken = await HEIMToken.deployed();
+    cocoAddress = cocoToken.address;
+    heimAddress = heimToken.address;
+  }
+  await factory.createPair(cocoAddress, heimAddress);
 };
--- migrations/3_deploy_heim.js
+++
@@ -1,5 +0,0 @@
-var HEIMToken = artifacts.require("./HEIMToken.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(HEIMToken, "HEIM Token", "HEIM", 18);
-};
